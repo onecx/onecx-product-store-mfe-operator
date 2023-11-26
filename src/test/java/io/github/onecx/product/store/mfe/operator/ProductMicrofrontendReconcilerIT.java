86db2b7d011b0503b535410193c56ba4cfeba0dc
Andrej Petras <andrej@lorislab.org>
2023-11-26 18:03:12 +0100

    tests: remove integration tests

--- src/test/java/io/github/onecx/product/store/mfe/operator/ProductMicrofrontendReconcilerIT.java
+++
@@ -1,8 +0,0 @@
-package io.github.onecx.product.store.mfe.operator;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ProductMicrofrontendReconcilerIT extends ProductMicrofrontendReconcilerTest {
-
-}
