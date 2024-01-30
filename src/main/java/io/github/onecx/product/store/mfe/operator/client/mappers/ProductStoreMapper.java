bfd321d1434cd342b2fba4330159f15ad558cdd4
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-01-30 16:52:32 +0100

    feat: fixed mapper (#14)

--- src/main/java/io/github/onecx/product/store/mfe/operator/client/mappers/ProductStoreMapper.java
+++ src/main/java/io/github/onecx/product/store/mfe/operator/client/mappers/ProductStoreMapper.java
@@ -1,6 +1,11 @@
 package io.github.onecx.product.store.mfe.operator.client.mappers;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import gen.io.github.onecx.product.store.mfe.v1.model.UpdateMfeRequest;
 import io.github.onecx.product.store.mfe.operator.MicrofrontendSpec;
@@ -8,5 +13,14 @@ import io.github.onecx.product.store.mfe.operator.MicrofrontendSpec;
 @Mapper
 public interface ProductStoreMapper {
 
+    @Mapping(target = "classifications", qualifiedByName = "setToString")
     UpdateMfeRequest map(MicrofrontendSpec spec);
+
+    @Named("setToString")
+    default String setToString(Set<String> classifications) {
+        if (classifications != null && !classifications.isEmpty()) {
+            return classifications.stream().map(Object::toString).collect(Collectors.joining(","));
+        }
+        return "";
+    }
 }
