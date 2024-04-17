355b55c01861df2bcd05e50473e3d2999b9ca115
Andrej Petras <andrej@lorislab.org>
2024-04-17 09:30:03 +0200

    feat: add mfe undeployed attributes

--- src/main/java/org/tkit/onecx/product/store/mfe/operator/client/mappers/ProductStoreMapper.java
+++ src/main/java/org/tkit/onecx/product/store/mfe/operator/client/mappers/ProductStoreMapper.java
@@ -1,6 +1,7 @@
 package org.tkit.onecx.product.store.mfe.operator.client.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.tkit.onecx.product.store.mfe.operator.MicrofrontendSpec;
 
 import gen.org.tkit.onecx.product.store.mfe.v1.model.UpdateMfeRequest;
@@ -8,6 +9,7 @@ import gen.org.tkit.onecx.product.store.mfe.v1.model.UpdateMfeRequest;
 @Mapper
 public interface ProductStoreMapper {
 
+    @Mapping(target = "undeployed", constant = "false")
     UpdateMfeRequest map(MicrofrontendSpec spec);
 
 }
